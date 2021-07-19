f3d8745a873097d3a27e44cd746b695f81244214
abluey <zoe1615@hotmail.com>
2021-07-19 21:04:58 +0800

    deleted client and changed everything to bot

--- Zoe/main.py
+++ Zoe/main.py
@@ -4,16 +4,15 @@ from discord.ext import commands
 
 from dotenv import load_dotenv
 
-client = discord.Client()
 bot = commands.Bot(command_prefix='!')
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f"Bot user {client.user} is ready.")
+    print(f"Bot user {bot.user} is ready.")
 
-@client.event
+@bot.event
 async def on_message(msg):
-    if msg.author == client.user:
+    if msg.author == bot.user:
         return
     
     if msg.content == "Ping":
@@ -31,4 +30,4 @@ async def hey(ctx, *, args):
 
 load_dotenv()
 BOT_TOKEN=os.environ['TOKEN']
-client.run(BOT_TOKEN)
+bot.run(BOT_TOKEN)
