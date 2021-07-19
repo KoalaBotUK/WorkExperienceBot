345a9fcc5b169dc7071368f9d622400ab51272da
James Flynn <jameswflynn@outlook.com>
2021-07-19 13:38:59 +0100

    Working bot

--- James/james-bot.py
+++ James/james-bot.py
@@ -1,15 +1,22 @@
 import discord
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 client = discord.Client()
+
+token = os.environ['DISCORD_TOKEN']
 
 @client.event
 async def on_ready():
-    print(f”Bot user {client.user} is ready.”)
+    print("Bot user" +client.user.name+" is ready.")
 
 @client.event
 async def on_message(msg):
     if msg.author == client.user:
         return                           # Don’t respond to itself
-    if msg.content == “Ping”:            # Check that the message content matches
-        await msg.channel.send(“Pong!”)  # If it does, reply
+    if msg.content == "Ping":            # Check that the message content matches
+        await msg.channel.send("Pong!")  # If it does, reply
 
-bot.run()
+client.run(token)
