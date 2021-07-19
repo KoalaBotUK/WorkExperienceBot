c0166b63368ed9d0e3ef29d87f32c0b68d606eb8
James Flynn <jameswflynn@outlook.com>
2021-07-19 13:56:39 +0100

    Added command with command prefix

--- James/james-bot.py
+++ James/james-bot.py
@@ -1,10 +1,11 @@
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-client = discord.Client()
+client = commands.Bot(command_prefix='!')
 
 token = os.environ['DISCORD_TOKEN']
 
@@ -12,11 +13,17 @@ token = os.environ['DISCORD_TOKEN']
 async def on_ready():
     print("Bot user" +client.user.name+" is ready.")
 
-@client.event
-async def on_message(msg):
-    if msg.author == client.user:
-        return                           # Don’t respond to itself
-    if msg.content == "Ping":            # Check that the message content matches
-        await msg.channel.send("Pong!")  # If it does, reply
+@client.command()
+async def hi(cxt, args):
+        if cxt.author == client.user:
+            return 
+        await cxt.channel.send("Hello "+ args +"!")
+
+#@client.event
+#async def on_message(msg):
+#    if msg.author == client.user:
+#        return                           # Don’t respond to itself
+#    if msg.content == "Ping":            # Check that the message content matches
+#        await msg.channel.send("Pong!")  # If it does, reply
 
 client.run(token)
